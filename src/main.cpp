28dfbac607c8cd9ce866e2cabb2ae9b1662cf134
Adam Brown <adamwbrown@gmail.com>
2020-08-09 00:45:11 -0700

    Switched to chai

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,9 @@
 #include <iostream>
-#include <sstream>
-#include <fstream>
-#include "wren.hpp"
+#include <chaiscript/chaiscript.hpp>
 
 using namespace std;
 
-static void writeFn(WrenVM *vm, const char *text)
-{
-	cout << text << endl;
-}
-
-void errorFn(WrenVM *vm, WrenErrorType errorType,
-			 const char *module, const int line,
-			 const char *msg)
-{
-	switch(errorType)
-	{
-		case WREN_ERROR_COMPILE:
-		{
-			printf("[%s line %d] [Error] %s\n", module, line, msg);
-		}
-			break;
-		case WREN_ERROR_STACK_TRACE:
-		{
-			printf("[%s line %d] in %s\n", module, line, msg);
-		}
-			break;
-		case WREN_ERROR_RUNTIME:
-		{
-			printf("[Runtime Error] %s\n", msg);
-		}
-			break;
-	}
-}
-
-std::string loadScript(const std::string& path)
+std::string loadScript(const std::string &path)
 {
 	std::ifstream t(path);
 	std::stringstream buffer;
@@ -43,39 +12,24 @@ std::string loadScript(const std::string& path)
 	return buffer.str();
 }
 
+string readLine()
+{
+	std::string line;
+	std::getline(std::cin, line);
+
+	return line;
+}
+
 int main()
 {
-	WrenConfiguration config;
-	wrenInitConfiguration(&config);
-	config.writeFn = &writeFn;
-	config.errorFn = &errorFn;
-	WrenVM *vm = wrenNewVM(&config);
+	chaiscript::ChaiScript chai;
+	chai.add(chaiscript::fun(&readLine), "readLine");
 
-	const char *module = "main";
-	const std::string script = loadScript("test.wren");
+	const string scriptFile = "test.chai";
+	const auto script = loadScript("test.chai");
 
-	WrenInterpretResult result = wrenInterpret(vm, module, script.c_str());
-
-	switch(result)
-	{
-		case WREN_RESULT_COMPILE_ERROR:
-		{
-			printf("Compile Error!\n");
-		}
-			break;
-		case WREN_RESULT_RUNTIME_ERROR:
-		{
-			printf("Runtime Error!\n");
-		}
-			break;
-		case WREN_RESULT_SUCCESS:
-		{
-			printf("Success!\n");
-		}
-			break;
-	}
-
-	wrenFreeVM(vm);
+	int i = chai.eval_file<int>(scriptFile);
+	cout << i << endl;
 
 	return 0;
 }
